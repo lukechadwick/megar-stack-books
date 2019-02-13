5b16f54310c77b07d86f4698b6a101b20eda4ed6
lukechadwick <llukechadwick@gmail.com>
2019-02-13 23:07:27 +1300

    add graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const graphqlHTTP = require('express-graphql');
 const app = express();
+
+// Pass graphql requests to express-graphql
+app.use('/graphql', graphqlHTTP({}));
 
 app.listen(4050, () => {
   console.log('now listening for requests on port 4050');
