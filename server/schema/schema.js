fec77f8008530a66e75a754220842167f8682749
lukechadwick <llukechadwick@gmail.com>
2019-02-15 20:01:20 +1300

    add dummy data and lodash dep

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
+const _ = reqire('lodash');
 
 const { GraphQLObjectType, GraphQLString } = graphql;
+
+let dummyData = [
+  { name: 'Book 1', genre: 'Fantasy', id: '1' },
+  { name: 'Book 2', genre: 'Sci-Fi', id: '2' },
+  { name: 'Book 3', genre: 'Action', id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -9,4 +16,23 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //
+        //code to get data from db
+        //
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
