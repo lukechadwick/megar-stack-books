1e8097ad3df8151afa222e06aad7ff9188979de5
lukechadwick <llukechadwick@gmail.com>
2019-02-17 22:29:46 +1300

    add book mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -79,13 +79,32 @@ const Mutation = new GraphQLObjectType({
   fields: {
     addAuthor: {
       type: AuthorType,
-      args: { name: { type: GraphQLString }, age: { type: GraphQLInt } },
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
       resolve(parent, args) {
         let author = new Author({
           name: args.name,
           age: args.age
         });
         return author.save();
+      }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        });
+        return book.save();
       }
     }
   }
