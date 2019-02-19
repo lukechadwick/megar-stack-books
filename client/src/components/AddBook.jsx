62963581bdfafdd3bac3f147882e70a5c3f90b87
lukechadwick <llukechadwick@gmail.com>
2019-02-20 00:19:33 +1300

    move queries to separate file

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
   constructor(props) {
@@ -53,6 +44,7 @@ class AddBook extends Component {
             </select>
           </div>
         </form>
+        <button>+</button>
       </React.Fragment>
     );
   }
